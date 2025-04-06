a76bc5e48d05f59ec2b8dca8e87b91455be926bd
simonacorreale <simona.correale.work@gmail.com>
2025-04-06 12:56:18 +0200

    first commit

---
+++ src/main/java/it/pizzeria/pizzeria_crud/PizzeriaCrudApplication.java
@@ -0,0 +1,13 @@
+package it.pizzeria.pizzeria_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/pizzeria/pizzeria_crud/PizzeriaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pizzeria.pizzeria_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

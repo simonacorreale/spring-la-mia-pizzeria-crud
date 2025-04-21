f750e9b72612d35afded4cbe5b93c13abf0a4973
simonacorreale <simona.correale.work@gmail.com>
2025-04-21 10:44:41 +0200

    feat: aggiunta 404 errore

--- src/main/java/it/pizzeria/pizzeria_crud/controller/MainController.java
+++ src/main/java/it/pizzeria/pizzeria_crud/controller/MainController.java
@@ -2,18 +2,20 @@
 package it.pizzeria.pizzeria_crud.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.pizzeria.pizzeria_crud.model.Pizza;
 import it.pizzeria.pizzeria_crud.repository.PizzaRepository;
 
 @Controller
-@RequestMapping("index")
+@RequestMapping("/index")
 public class MainController {
 
     @Autowired
@@ -25,8 +27,22 @@ public class MainController {
         List<Pizza> result = pizzaRepository.findAll();
         model.addAttribute("list", result);
 
-        return "index";
+        return "pizza/index";
 
+    }
+
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        Optional<Pizza> optPizza = pizzaRepository.findById(id);
+
+        if (optPizza.isPresent()) {
+            model.addAttribute("pizza", pizzaRepository.findById(id).get());
+
+        } else {
+            return "redirect:/error/404";
+        }
+
+        return "pizza/show";
     }
 
 }
